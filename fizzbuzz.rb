e2ff7634c3c71e3e65fdd9c7b02419c8c98145d1
Jessica Dior <jessicaclairdior@gmail.com>
2020-08-11 22:08:31 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,11 +1,11 @@
 def fizzbuzz(int)
 
   if int % 3 == 0
-  puts  "Fizz"
+  puts "Fizz"
      int % 5 == 0
-  puts    "Buzz"
+  puts "Buzz"
       int % 15 == 0
-    puts    "FizzBuzz"
+    puts "FizzBuzz"
 end
 
 end
